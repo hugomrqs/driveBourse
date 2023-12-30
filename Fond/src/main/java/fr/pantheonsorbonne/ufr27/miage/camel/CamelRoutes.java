f8527d8fec5a9b778433e24721ae1a01463402d6
hugomrqs <hugo.albert.marques@gmail.com>
2023-12-30 16:58:20 +0100

    camelroutes

---
+++ Fond/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/CamelRoutes.java
@@ -0,0 +1,12 @@
+package fr.pantheonsorbonne.ufr27.miage.camel;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.builder.RouteBuilder;
+
+@ApplicationScoped
+public class CamelRoutes extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
