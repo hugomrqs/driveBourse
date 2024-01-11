3bb84b3e47e8675cf50fb7309d427e374318b6e0
hugo.albert.marques@gmail.com <hugo.albert.marques@gmail.com>
2024-01-11 22:27:33 +0100

    modif pour le chef

---
+++ Entreprise/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/BusinessModel.java
@@ -0,0 +1,63 @@
+package fr.pantheonsorbonne.ufr27.miage.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "BusinessModel")
+public class BusinessModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDBusinessModel")
+    private Integer idBusinessModel;
+
+    @JoinColumn(name = "SiretStartUP", referencedColumnName = "SiretStartUP")
+    private String siretStartUp;
+
+    @Column(name = "argentLeveeXpTasvee")
+    private Integer argentLeveeXpTasvee;
+
+    @Column(name = "partCedeeXpTasvee")
+    private Integer partCedeeXpTasvee;
+
+    public BusinessModel() {
+    }
+
+    public BusinessModel(Integer argentLeveeXpTasvee,
+                         Integer partCedeeXpTasvee,
+                         String siretStartUp) {
+        this.argentLeveeXpTasvee = argentLeveeXpTasvee;
+        this.partCedeeXpTasvee = partCedeeXpTasvee;
+        this.siretStartUp = siretStartUp;
+    }
+
+    public Integer getIdBusinessModel() {
+        return idBusinessModel;
+    }
+
+    public void setIdBusinessModel(Integer idBusinessModel) {
+        this.idBusinessModel = idBusinessModel;
+    }
+    public String getsiretStartUp() {
+        return siretStartUp;
+    }
+
+    public void setsiretStartUp(String siretStartUp) {
+        this.siretStartUp = siretStartUp;
+    }
+
+    public Integer getArgentLeveeXpTasvee() {
+        return argentLeveeXpTasvee;
+    }
+
+    public void setArgentLeveeXpTasvee(Integer argentLeveeXpTasvee) {
+        this.argentLeveeXpTasvee = argentLeveeXpTasvee;
+    }
+
+    public Integer getPartCedeeXpTasvee() {
+        return partCedeeXpTasvee;
+    }
+
+    public void setPartCedeeXpTasvee(Integer partCedeeXpTasvee) {
+        this.partCedeeXpTasvee = partCedeeXpTasvee;
+    }
+}
